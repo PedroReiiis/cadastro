ba1731422ea9fa4b1df8a1616dd793e31a5c3b95
PedroReiiis <cryspe2@gmail.com>
2025-03-14 22:28:35 -0400

    atualização 14/03

--- cep.js
+++ cep.js
@@ -1,6 +1,7 @@
 function consultarCEP() {
     const cep = document.getElementById('cep').value.replace('-', '');
     const resultado = document.getElementById('resultado');
+	const numr = document.getElementById('numr');
 
     //limpas resultados anteriores
     document.getElementById('logradouro').textContent = '';
@@ -28,10 +29,42 @@ function consultarCEP() {
                 document.getElementById('localidade').textContent = data.localidade;
                 document.getElementById('uf').textContent = data.uf;
                 resultado.style.display = 'block';
+				numr.style.display = '';
             }
         })
         .catch(error => {
             alert('Erro ao consultar CEP');
             console.error('Erro:', error)
         });
+}
+
+// adicionar endereço 
+
+function confirmarEndr(){
+	const numero = document.getElementById('numero').value;
+	if (numero.length === 0) {
+        alert('digite um número valido.');
+        return;
+    }
+	const resultado = document.getElementById('resultado');
+	console.log(resultado);
+	console.log(document.getElementById('logradouro').textContent);
+	console.log(document.getElementById('bairro').textContent);
+	console.log(document.getElementById('localidade').textContent);
+	console.log(document.getElementById('uf').textContent);
+	console.log(document.getElementById('numero').value);
+	
+	const cep = document.getElementById('cep').value.replace('-', '');
+	const endr = 'Endereço: ' + document.getElementById('logradouro').textContent 
+	+ ', Número: ' + document.getElementById('numero').value
+	+ ', Bairro: ' + document.getElementById('bairro').textContent
+	+ ', Cidade: ' + document.getElementById('localidade').textContent 
+	+ ' - ' + document.getElementById('uf').textContent
+	+ ' CEP:' + cep;
+	
+	console.log(endr);
+
+    window.opener.document.getElementById('cep').value = endr;
+	window.close();
+
 }
